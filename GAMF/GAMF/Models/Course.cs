8476b0009a5d3718069c35ba83d1a0aec3c77bfc
Törőcsik Norbert(NE67MX) <128035248+Cirionhu@users.noreply.github.com>
2024-01-03 12:03:13 +0100

    Bead

---
+++ GAMF/GAMF/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace GAMF.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        [Display(Name = "Tárgy megnevezése")]
+        [Required(ErrorMessage = "Tárgy megnevezése kitöltése kötelező!")]
+        public string Title { get; set; }
+        [Display(Name = "Tárgy kreditszám")]
+        [Required(ErrorMessage = "Tárgy kreditszám kitöltése kötelező!")]
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+}
